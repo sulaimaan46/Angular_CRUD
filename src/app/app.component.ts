e02c70a8ab2a952ea881a706a64f2a824ba03552
sulai46 <sulaiman@sparkouttech.com>
2022-03-21 17:32:02 +0530

    Crud Commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CrudService } from './crud.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCRUD';
+  todoList:any = [];
+
+  constructor(private crudService: CrudService){}
+
+  ngOnInit(): void {
+    this.listTodos();
+  }
+
+  listTodos(){
+    this.crudService.list().subscribe((response)=>{
+      this.todoList = response;
+    },(error=>{
+
+    }));
+  }
+
+  createTodo(){
+    let todo = {
+      id: new Date().getTime(),
+      title:`Some Todo`
+    }
+    this.crudService.create(todo).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+
+    }));
+  }
+
+  editTodo(todo: any){
+    let data = {
+      id: new Date().getTime(),
+      title:`Some Todo`
+    }
+    this.crudService.update(todo.id,data).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+
+    }));
+  }
+
+  deleteTodo(id: any){
+    this.crudService.delete(id).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+    }));
+  }
 }
